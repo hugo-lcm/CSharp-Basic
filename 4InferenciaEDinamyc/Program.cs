0e2f4b373bc277170e2551efb4a3d65f1570d0aa
hugo-lcm <hugolcm@live.com>
2022-11-18 20:41:29 -0300

    4 - inferência e dinamyc

---
+++ 4InferenciaEDinamyc/Program.cs
@@ -0,0 +1,11 @@
+﻿// inferência
+int number1 = 1;
+var number2 = 2;
+var test = "test";
+Console.WriteLine($"{number1}, {number2}, {test}");
+
+// dynamic
+dynamic myVar = 2; // ocupa mais espaço na memória
+Console.WriteLine(myVar.GetType());
+myVar = "test2";
+Console.WriteLine(myVar.GetType());
