ffe4fd6f000f2288d2410afeaf30d920414bf625
hugo-lcm <hugolcm@live.com>
2022-11-18 20:12:28 -0300

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿// passagem por valor
+int n1 = 3;
+int n2 = n1; // n2 recebe uma cópia de n1, logo o valor de n2 não é alterado quando n1 muda
+n1 = 5;
+Console.WriteLine($"number 1 = {n1}, number 2 = {n2}"); // 5 3
+
+// passagem por referência
+Test t1 = new Test();
+t1.Year = 2022;
+Test t2 = t1;
+t1.Year = 2023;
+Console.WriteLine($"t1.Year = {t1.Year}, t2.Year = {t2.Year}"); // 2023 2023
+
+
+class Test
+{
+    public int Year;
+}
