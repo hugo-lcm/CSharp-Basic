d4800e7d8349621ee769d0b450f3a6b0d1394c20
hugo-lcm <hugolcm@live.com>
2022-11-27 19:22:19 -0300

    7 - operadores aritiméticos

---
+++ 7OperadoresAritimeticos/Program.cs
@@ -0,0 +1,26 @@
+﻿int x = 11;
+int y = 2;
+
+// adição
+Console.WriteLine(x + y);
+
+// subtração
+Console.WriteLine(x - y);
+
+// multiplicação
+Console.WriteLine(x * y);
+
+// divisao
+Console.WriteLine(x / y); // o resto é descartado
+
+// modulo
+Console.WriteLine(x % y);
+
+// concatenação de strings
+Console.WriteLine("abc" + "def");
+
+// operadores reduzidos
+Console.WriteLine(x += 1);
+Console.WriteLine(x -= 1);
+Console.WriteLine(x *= 1);
+Console.WriteLine(x /= 1);
