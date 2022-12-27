1f98072ea63f8402813af0d1e136f24016eeec38
hugo-lcm <hugolcm@live.com>
2022-12-27 10:44:34 -0300

    25 - herança

---
+++ 25Heranca/PessoaFisica.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _25Heranca
+{
+    public class PessoaFisica : Cliente
+    {
+        public int Cpf { get; set; }
+
+        public PessoaFisica(string nome, int cpf, string endereco, string telefone, string status, int codigo)
+        {
+            this.Nome = nome;
+            this.Cpf = cpf;
+            this.Endereco = endereco;
+            this.Telefone = telefone;
+            this.Status = status;
+            this.Codigo = codigo;
+        }
+
+        public bool Incluir()
+        {
+            Status = $"{Cpf} incluído";
+            return true;
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine($"nome: {Nome}\n" +
+                $"cpf: {Cpf}\n" +
+                $"endereço: {Endereco}\n" +
+                $"telefone: {Telefone}\n" +
+                $"status: {Status}\n" +
+                $"codigo: {Codigo}");
+        }
+    }
+}
---
+++ 25Heranca/Program.cs
@@ -0,0 +1,10 @@
+﻿using _25Heranca;
+
+Console.WriteLine("-=-=-= herança =-=-=-\n");
+
+/* no C# não existe herança múltipla, ou seja, cada classe só pode ter um pai. Entretanto, 
+uma classe pode ter uma ou mais classes filhas.*/
+
+PessoaFisica pf = new PessoaFisica("fulano", 123456789, "centro", "35261258", "ativo", 1);
+Console.WriteLine(pf.Incluir());
+pf.ShowInfo();
