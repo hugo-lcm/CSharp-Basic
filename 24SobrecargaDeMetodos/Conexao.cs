244756a65044aea1c8b1059a2d27b989e4eed652
hugo-lcm <hugolcm@live.com>
2022-12-27 09:33:27 -0300

    24 - sobrecarga de métodos

---
+++ 24SobrecargaDeMetodos/Conexao.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24SobrecargaDeMetodos
+{
+    internal class Conexao
+    {
+        /*
+        class Exemplo
+        {
+            1 - Variáveis/Campos
+            2 - Propriedades
+            3 - Métodos
+        }
+        */
+        private string Status { get; set; }
+
+        public Conexao()
+        {
+            Status = "construtor padrão";
+        }
+
+        public bool Conectar()
+        {
+            Status = "conectado com sucesso";
+            return true;
+        }
+
+        public bool Conectar(string banco)
+        {
+            Status = "conectado com sucesso";
+            return true;
+        }
+
+        public bool Conectar(string banco, string nome, string senha)
+        {
+            Status = "conectado com sucesso";
+            return true;
+        }
+
+        public bool Desconectar()
+        {
+            Status = "deconectado com sucesso";
+            return true;
+        }
+    }
+}
---
+++ 24SobrecargaDeMetodos/Program.cs
@@ -0,0 +1,14 @@
+﻿using _24SobrecargaDeMetodos;
+
+Console.WriteLine("-=-=-= sobrecarga de métodos =-=-=-\n");
+
+/* no C# pode haver, dentro de uma mesma classe, métodos com nomes iguais, mas com parâmetros 
+diferentes. É o que chamamos de sobrecarga de métodos. Basta que a assinatura do método seja 
+diferente. A assinatura do método significa a combinação do nome do método e seus parâmetros.
+É possível utilizar sobrecarga de métodos para métodos comuns e não somente para construtores.
+Sendo os parâmetros diferentes, seja quanto ao tipo ou a quantidade as assinaturas serão diferentes.*/
+
+Conexao c1 = new Conexao();
+Console.WriteLine(c1.Conectar());
+Console.WriteLine(c1.Conectar("nubank"));
+Console.WriteLine(c1.Conectar("nubank", "fulano", "1234"));
