3bbf48abe5eb8568c58ec35b8520e34d90cd6f34
hugo-lcm <hugolcm@live.com>
2022-12-27 15:32:22 -0300

    26 - encapsulamento

---
+++ 26Encapsulamento/PessoaJuridica.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _26Encapsulamento
+{
+    public class PessoaJuridica : Cliente
+    {
+        private int Cnpj { get; set; }
+
+        public PessoaJuridica(string nome, int codigo, int cnpj)
+        {
+            Nome = nome;
+            Status = "valor inicial";
+            Codigo = codigo;
+            Cnpj = cnpj;
+        }
+
+        public void Incluir()
+        {
+            Status = $"{Cnpj} incluído";
+        }
+    }
+}
