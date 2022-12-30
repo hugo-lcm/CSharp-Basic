65ef18eeffd6d7d2730c2c50a2df878113d88844
hugo-lcm <hugolcm@live.com>
2022-12-30 17:54:32 -0300

    34 - structs

---
+++ 34Structs/Carro.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _34Structs
+{
+    internal class Carro
+    {
+        private List<Dono> antigosDonos;
+
+        public Carro()
+        {
+            this.antigosDonos = new List<Dono>();
+        }
+
+        public void AdicionarAntigoDono(Dono dono)
+        {
+            antigosDonos.Add(dono);
+        }
+
+        public List<Dono> GetAntigosDonos()
+        {
+            return antigosDonos;
+        }
+    }
+
+
+}
