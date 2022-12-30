a21bc81d9613d6a6d93b4de7dbf4e9379e87ec9f
hugo-lcm <hugolcm@live.com>
2022-12-30 15:29:43 -0300

    32 - métodos abstratos

---
+++ 32MetodosAbstratos/Cliente.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _32MetodosAbstratos
+{
+    public abstract class Cliente
+    {
+        public string Nome { get; set; }
+        public int Codigo { get; set; }
+
+        public abstract string AlterarNome(string nome);
+    }
+}
---
+++ 32MetodosAbstratos/Program.cs
@@ -0,0 +1,24 @@
+﻿using _32MetodosAbstratos;
+
+Console.WriteLine("-=-=-= métodos abstratos =-=-=-\n");
+
+/* um método abstrato não tem corpo, apenas a declaração do método. Ele é utilizado para 
+determinar que as classes que estenderem uma determinada classe pai deverão implementar 
+determinado método, porém, a classe pai em si não possui essa implementação. No exemplo 
+abaixo foi adicionado 2 métodos abstratos (Alterar e Incluir):
+
+public abstract void Alterar();
+public abstract void Incluir();
+
+obs: eles podem ter parâmetros na assinatura
+
+isto significa que as classes derivadas da classe Cliente terão necessariamente que implementar 
+estes 2 métodos (Alterar e Incluir), bem como temos que mudar a classe para classe abstrata, 
+porque se você tentar compilar o código, ou seja, somente classes abstratas podem conter métodos 
+abstratos*/
+
+
+PessoaFisica pf = new PessoaFisica("fulano", 12345);
+Console.WriteLine(pf.Nome);
+pf.AlterarNome("ciclano");
+Console.WriteLine(pf.Nome);
