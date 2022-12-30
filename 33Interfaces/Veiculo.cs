2609ce65f338b24a3267e8288b2c2bfc3fd655e4
hugo-lcm <hugolcm@live.com>
2022-12-30 16:19:45 -0300

    33 - interfaces

---
+++ 33Interfaces/Veiculo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _33Interfaces
+{
+    // se for usar herança e interface, herança vem primeiro, após os :
+    public abstract class Veiculo : IVeiculo
+    {
+        public abstract string Ligar();
+
+        public abstract string Desligar();
+
+        public abstract string Abastecer();
+
+        public string Desabasatecer()
+        {
+            return "o veículo foi desabastecido";
+        }
+    }
+}
